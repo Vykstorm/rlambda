3b066ccf430b8867a1c7f86358c087f40ba5c460
Vykstorm <victorruizgomezdev@gmail.com>
2018-12-14 12:51:27 +0100

    Added setup.py and gitignore

---
+++ setup.py
@@ -0,0 +1,22 @@
+
+'''
+Installation script
+'''
+
+from setuptools import setup
+
+
+if __name__ == '__main__':
+    setup(
+        name = 'rlambda',
+        version = '1.0.0',
+        description = 'Python module that allows to generate lambda functions recursively',
+        author = 'Vykstorm',
+        author_email = 'victorruizgomezdev@gmail.com',
+        python_requires = '>=2.7',
+        install_requires = [],
+        dependency_links = [],
+        package_dir= {'rlambda':'src'},
+        packages= ['rlambda'],
+        keywords = ['lambda', 'ast']
+    )
