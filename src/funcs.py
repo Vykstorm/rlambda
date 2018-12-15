ca1b6c50ee378a426b3cf05fc4738c3d8f090bbf
Vykstorm <victorruizgomezdev@gmail.com>
2018-12-15 13:50:15 +0100

    Refactorized code

--- src/funcs.py
+++ src/funcs.py
@@ -17,44 +17,82 @@ _funcs.update(math.__dict__)
 _builtins = SimpleNamespace(**_funcs)
 
 
-def _build_wrappers(*args):
+def _build_wrapper(func):
+    assert callable(func)
+
     isinstance = _builtins.isinstance
     any, map, str = _builtins.any, _builtins.map, _builtins.str
     hasattr, getattr = _builtins.hasattr, _builtins.getattr
 
-    def _build_wrapper(funcname):
-        assert isinstance(funcname, str) and hasattr(_builtins, funcname)
-        func = getattr(_builtins, funcname)
+    @wraps(func)
+    def _wrapper(*args, **kwargs):
+        if any(map(lambda x: isinstance(x, RLambda), chain(args, kwargs.values()))):
+            return RLambda._call_op(func, *args, **kwargs)
+        return func(*args, **kwargs)
 
-        @wraps(func)
-        def _wrapper(*args, **kwargs):
-            if any(map(lambda x: isinstance(x, RLambda), chain(args, kwargs.values()))):
-                return RLambda._call_op(func, *args, **kwargs)
-            return func(*args, **kwargs)
-
-        globals()[funcname] = _wrapper
-
-    for arg in args:
-        _build_wrapper(arg)
+    return _wrapper
 
 
 # Built-in wrappers
-_build_wrappers(
-    'len', 'int', 'float', 'bool', 'str', 'complex', 'list', 'tuple', 'set', 'frozenset',
-    'max', 'min'
-)
+
+len = _build_wrapper(_builtins.len)
+int = _build_wrapper(_builtins.int)
+float = _build_wrapper(_builtins.float)
+bool = _build_wrapper(_builtins.bool)
+str = _build_wrapper(_builtins.str)
+complex = _build_wrapper(_builtins.complex)
+list = _build_wrapper(_builtins.list)
+tuple = _build_wrapper(_builtins.tuple)
+set = _build_wrapper(_builtins.set)
+frozenset = _build_wrapper(_builtins.frozenset)
+min = _build_wrapper(_builtins.min)
+max = _build_wrapper(_builtins.max)
+
+
 
 # Math function wrappers
-_build_wrappers(
-    'ceil', 'copysign', 'fabs', 'factorial', 'floor', 'fmod', 'frexp', 'fsum', 'gcd',
-    'isclose', 'isfinite', 'isinf', 'isnan', 'ldexp', 'modf', 'trunc',
 
-    'exp', 'expm1', 'log', 'log1p', 'log2', 'pow', 'sqrt',
+ceil = _build_wrapper(_builtins.ceil)
+copysign = _build_wrapper(_builtins.copysign)
+fabs = _build_wrapper(_builtins.fabs)
+factorial = _build_wrapper(_builtins.factorial)
+floor = _build_wrapper(_builtins.floor)
+fmod = _build_wrapper(_builtins.fmod)
+frexp = _build_wrapper(_builtins.frexp)
+fsum = _build_wrapper(_builtins.fsum)
+gcd = _build_wrapper(_builtins.gcd)
+isclose = _build_wrapper(_builtins.isclose)
+isfinite = _build_wrapper(_builtins.isfinite)
+isinf = _build_wrapper(_builtins.isinf)
+isnan = _build_wrapper(_builtins.isnan)
+ldexp = _build_wrapper(_builtins.ldexp)
+modf = _build_wrapper(_builtins.modf)
+trunc = _build_wrapper(_builtins.trunc)
+exp = _build_wrapper(_builtins.exp)
+expm1 = _build_wrapper(_builtins.expm1)
+log = _build_wrapper(_builtins.log)
+log1p = _build_wrapper(_builtins.log1p)
+log2 = _build_wrapper(_builtins.log2)
+pow = _build_wrapper(_builtins.pow)
+sqrt = _build_wrapper(_builtins.sqrt)
+acos = _build_wrapper(_builtins.acos)
+asin = _build_wrapper(_builtins.asin)
+atan = _build_wrapper(_builtins.atan)
+atan2 = _build_wrapper(_builtins.atan2)
+cos = _build_wrapper(_builtins.cos)
+hypot = _build_wrapper(_builtins.hypot)
+sin = _build_wrapper(_builtins.sin)
+tan = _build_wrapper(_builtins.tan)
+acosh = _build_wrapper(_builtins.acosh)
+asinh = _build_wrapper(_builtins.asinh)
+atanh = _build_wrapper(_builtins.atanh)
+cosh = _build_wrapper(_builtins.cosh)
+sinh = _build_wrapper(_builtins.sinh)
+tanh = _build_wrapper(_builtins.tanh)
+degrees = _build_wrapper(_builtins.degrees)
+radians = _build_wrapper(_builtins.radians)
+erf = _build_wrapper(_builtins.erf)
+erfc = _build_wrapper(_builtins.erfc)
+gamma = _build_wrapper(_builtins.gamma)
+lgamma = _build_wrapper(_builtins.lgamma)
 
-    'acos', 'asin', 'atan', 'atan2', 'cos', 'hypot', 'sin', 'tan',
-    'acosh', 'asinh', 'atanh', 'cosh', 'sinh', 'tanh',
-
-    'degrees', 'radians',
-
-    'erf', 'erfc', 'gamma', 'lgamma'
-)
