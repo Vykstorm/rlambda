149f35b34e040b308a8c6aa1546d171ba23e4d27
Vykstorm <victorruizgomezdev@gmail.com>
2018-12-21 00:04:21 +0100

    Modified facade of this module

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,6 @@
 
+# This script declares the classes and methods exposed to the outside of this module
 
-from .rlambda import RLambda
+from .rlambda import RLambda, RLambdaIdentity, bind
+from .formatter import RLambdaFormatter, DefaultRLambdaFormatter
+from .funcs import *
